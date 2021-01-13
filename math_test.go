33713633551499335d45e0aed9e5f00ff5d73d79
Henrique Ramalho <hrd.ramalho@gmail.com>
2021-01-12 22:51:22 -0300

    feat(proposital bug): insert a proposital fail on test

--- math_test.go
+++ math_test.go
@@ -6,7 +6,7 @@ func TestSum(t *testing.T) {
 
 	total := sum(15, 15)
 
-	if total != 30 {
+	if total != 999 {
 		t.Errorf("Resultado da soma é inválido: Resultado %d. Esperado: %d", total, 30)
 	}
 }
